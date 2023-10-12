b2d0a0b878e0fc93aa6fdd6a300c521125d12207
Jakub Wieja <130178508+WiejWay@users.noreply.github.com>
2023-10-12 21:59:50 +0200

    e3

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploaded_file'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formularz wysyłania plewiwkewwów</title>
+    <title>Formularz wysyłania</title>
      <style>
         body {
             font-family: Arial, sans-serif;
