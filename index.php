afe9493712b09fff6da8a96c9ee55c8bdba35a40
Jakub Wieja <130178508+WiejWay@users.noreply.github.com>
2023-10-30 23:59:47 +0100

    e5

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploaded_file'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>awwwwwwwwwwwwwwwwwwwwaaaawdfdfaaaaaaaaaaaaawwwwww</title>
+    <title>awwwwwwwwwwwwwwwwwwwwaaaawdfdfaaaaaaawwwwwwwwwwwwasdaaaaaaawwwwww</title>
      <style>
         body {
             font-family: Arial, sans-serif;
