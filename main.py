4131dbe0c22fafc594f446c220e11343922224ea
Jakub Wieja <130178508+WiejWay@users.noreply.github.com>
2023-11-29 23:12:52 +0100

    uyggyuo

--- main.py
+++ main.py
@@ -23,9 +23,8 @@ nlkj;;;jjnjnojno
     # Wykonaj OCR na zrzucie ekranu
     text = tess.image_to_string(screenshot)
     filtered_text = ''.join(char for char in text if char.isdigit() or char == '/')
-kkkkkkkkkkllllllllllllllljnnnnnnnnn
+kkkkkkkkkklllllllllllllll
     try:
-        current_read, total = map(int, filtered_text.split('/'))kkkkkkkkkkk
 
         # Sprawdź czy poprzedni odczyt był zdefiniowany i czy obecny odczyt spadł poniżej 50%
         if previous_read is not None and current_read/total <= 0.5:
